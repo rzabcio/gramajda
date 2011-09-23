bfe86a4787655e0ef7c843c1ad100aa1c88da356
rzabcio <rzabcio@gmail.com>
2011-09-23 19:26:51 +0200

    First boardgames table created from JSON AJAX request.

--- grajteka/static/js/application.js
+++ grajteka/static/js/application.js
@@ -28,3 +28,17 @@ $(document).ready(function(){
 
 });
 
+function boardgamesJsonToTable(data) {
+	var items = [];
+	$.each(data, function() {
+		items.push('<tr>'
+			+ '<td><a href=\"/boardgame/' + this.fields.meta + '/\">' + this.fields.meta + '</a></td>'
+			+ '<td><a href=\"/user/' + this.fields.owner + '/\">' + this.fields.owner + '</a></td>'
+			+ '<td><a href=\"/user/' + this.fields.patron + '/\">' + this.fields.patron + '</a></td>'
+			+ '<td><a href=\"/user/' + this.fields.holder + '/\">' + this.fields.holder + '</a></td>'
+			+ '</tr>');
+	});
+	return '<table class=\"zebra-striped\"><tbody><tr><th>Nazwa gry</th><th>Właściciel</th><th>Opiekun</th><th>Posiadający</th></tr>' + items + '</tbody></table>';
+	//return '<table class=\"zebra-striped\"><tbody>' + items + '</tbody></table>';
+}
+
